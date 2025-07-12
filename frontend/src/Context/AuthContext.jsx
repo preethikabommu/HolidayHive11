7069591e6366d6077947d3a0113663de4bd76fd6
preethikabommu <satyapreethika@gmail.com>
2025-07-12 22:48:01 +0530

    Update AuthContext.jsx

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react'
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    const serverUrl = "http://localhost:8000"
+    const serverUrl = "https://holidayhive11-backend.onrender.com"
 
     let [loading,setLoading]=useState(false)
 
